18d957ffd3c4f97afa309941f05b0631ceabcde8
Nguyễn <nguyendinhphu2909@gmail.com>
2019-08-18 22:23:53 +0700

    Initial Commit

---
+++ GetFramesVideo/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  GetFramesVideo
+//
+//  Created by NDPhu on 8/18/19.
+//  Copyright © 2019 iosDVL. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
